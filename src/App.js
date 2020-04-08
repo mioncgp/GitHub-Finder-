05ef16183978029e14dabdd252637218907e6863
Mike <mioncgp@gmail.com>
2020-04-08 11:52:46 +0400

    adding func

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search';
 
 class App extends React.Component {
 
@@ -15,15 +16,18 @@ class App extends React.Component {
     console.log(process.env.REACT_APP_GITHUB_CLIENT_ID)
     this.setState({ loading: true })
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    console.log(res.data);
     this.setState({ users: res.data, loading: false })
   }
 
   render() {
+    const { users, loading } = this.state;
     return  (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users users={this.state.users} loading={this.state.loading}/>
+          <Search />
+          <Users users={users} loading={loading}/>
         </div>
       </div>
     );
